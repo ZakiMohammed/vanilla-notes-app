bb42e18fd4ad05bf3896f998dbb22d178a46752a
Zaki Shaikh <zaki-shaikh@outlook.com>
2022-03-20 17:08:48 +0530

    Fetch API impmented

--- js/app.js
+++ js/app.js
@@ -1,34 +1,65 @@
 const dvNotesList = document.querySelector('.notes-list')
 const dvNotesEmpty = document.querySelector('.notes-empty')
+const dvLoader = document.querySelector('.loader-holder')
 const txtNotes = document.getElementById('txtNotes')
 const btnAdd = document.getElementById('btnAdd')
 
-const onRemove = function (e, id) {
-    const index = notes.findIndex(i => i.id == id)
-    notes.splice(index, 1)
+// const apiUrl = `https://jsonplaceholder.typicode.com/todos/`
+const apiUrl = `https://vanillax-todo-api.herokuapp.com/api/todos/`
 
-    const dvNotesItem = e.closest('.notes-item')
-    dvNotesItem.remove()
+let notes = []
 
-    if (notes.length == 0) {
-        dvNotesEmpty.removeAttribute('hidden')
-    }
+const onRemove = function (e, _id) {
+
+    // remove note from server
+    showLoader()
+    fetch(apiUrl + _id, {
+        method: 'DELETE'
+    })
+        .then(response => response.json())
+        .then(() => {
+            const index = notes.findIndex(i => i._id == _id)
+            notes.splice(index, 1)
+
+            const dvNotesItem = e.closest('.notes-item')
+            dvNotesItem.remove()
+
+            if (notes.length == 0) {
+                dvNotesEmpty.removeAttribute('hidden')
+            }
+        })
+        .catch(error => {
+            alert('Something went wrong')
+        })
+        .finally(() => hideLoader())
 }
+const showLoader = () => dvLoader.style.display = 'flex'
+const hideLoader = () => dvLoader.style.display = 'none'
 
-const notes = []
+// get notes listing
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        notes = data
 
-for (const note of notes) {
-    dvNotesList.innerHTML += `
-        <div class="notes-item">
-            <div><button class="btn-remove" onclick="onRemove(this, ${note.id})"><i class="fa-solid fa-trash-can"></i></button></div>
-            <p>${note.body}</p>
-        </div>
-    `
-}
+        for (const note of notes) {
+            dvNotesList.innerHTML += `
+                <div class="notes-item">
+                    <div><button class="btn-remove" onclick="onRemove(this, '${note._id}')"><i class="fa-solid fa-trash-can"></i></button></div>
+                    <p>${note.title}</p>
+                </div>
+            `
+        }
 
-if (notes.length == 0) {
-    dvNotesEmpty.removeAttribute('hidden')
-}
+        // for empty message
+        if (notes.length == 0) {
+            dvNotesEmpty.removeAttribute('hidden')
+        }
+    })
+    .catch(error => {
+        alert('Something went wrong')
+    })
+    .finally(() => hideLoader())
 
 btnAdd.addEventListener('click', function (e) {
     e.preventDefault()
@@ -38,25 +69,43 @@ btnAdd.addEventListener('click', function (e) {
         return
     }
 
-    const note = {
-        id: parseInt((Math.random() * 10000).toFixed(0)),
-        body: txtNotes.value
+    let note = {
+        // _id: parseInt((Math.random() * 10000).toFixed(0)),
+        title: txtNotes.value
     }
 
-    notes.unshift(note)
+    // add to server
+    showLoader()
+    fetch(apiUrl, {
+        method: 'POST',
+        body: JSON.stringify(note),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            note = data
 
-    const noteElement = `
-        <div class="notes-item">
-            <div><button class="btn-remove" onclick="onRemove(this, ${note.id})"><i class="fa-solid fa-trash-can"></i></button></div>
-            <p>${note.body}</p>
-        </div>
-    `
+            notes.unshift(note)
 
-    dvNotesList.innerHTML = noteElement + dvNotesList.innerHTML
+            const noteElement = `
+                <div class="notes-item">
+                    <div><button class="btn-remove" onclick="onRemove(this, '${note._id}')"><i class="fa-solid fa-trash-can"></i></button></div>
+                    <p>${note.title}</p>
+                </div>
+            `
 
-    txtNotes.value = ''
-    
-    if (notes.length != 0) {
-        dvNotesEmpty.setAttribute('hidden', '')
-    }
+            dvNotesList.innerHTML = noteElement + dvNotesList.innerHTML
+
+            txtNotes.value = ''
+
+            if (notes.length != 0) {
+                dvNotesEmpty.setAttribute('hidden', '')
+            }
+        })
+        .catch(error => {
+            alert('Something went wrong')
+        })
+        .finally(() => hideLoader())
 })
